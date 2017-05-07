69a23b818637cc7b8dbc77f1c22168236204f335
WalterBrito <waltter.britto@gmail.com>
2017-05-07 15:56:39 -0300

    django tutorial

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/5
+    # detail(request=<HttpRequest object>, question_id='34')
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results
+    # detail(request=<HttpRequest object>, question_id='34')
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote
+    # detail(request=<HttpRequest object>, question_id='34')
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
